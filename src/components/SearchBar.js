69653eac0b4ba218c4b506fc0b3e8fe8a1e80798
DHARMESH PODDAR <dharmeshpoddar3@gmail.com>
2019-05-26 11:30:10 +0530

    initial commit without search bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class SearchBar extends React.Component{
+   render(){
+
+       return(
+          <div>
+             kaam kr le kuch
+          </div>
+       );
+
+
+
+   }
+
+}
+export default SearchBar;
