4ef3365c8bdfffcc6f39c6e38f1f341ed6709cce
DHARMESH PODDAR <dharmeshpoddar3@gmail.com>
2019-05-28 22:52:50 +0530

    stucked in api

---
+++ src/components/VideoItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const VideoItem = props =>{
+   
+    return(
+        <div>
+            video List
+        </div>
+    );
+
+
+}
+export default VideoItem;
--- src/components/apis/youtube.js
+++ src/components/apis/youtube.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const KEY='AIzaSyBrZtiGlesXgPCb42QVQMKJ9yC7Ss955Kc' ;
+
+export default axios.create({
+
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params:{
+        part:'snippet',
+        maxResults:5,
+        key:KEY
+
+    }
+
+
+});
